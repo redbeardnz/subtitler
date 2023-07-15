0a2b8ca6350739d524129a24886e085fdd51655c
redbeard <developer.bannon@gmail.com>
2023-07-16 11:17:44 +1200

    disable container network by default
    Signed-off-by: redbeard <developer.bannon@gmail.com>

--- asr.sh
+++ asr.sh
@@ -6,7 +6,7 @@ SCRIPT_NAME=$(basename ${0})
 
 function show_usage {
     echo "Usage: ${SCRIPT_NAME} -h   show this help info."
-    echo "       ${SCRIPT_NAME} [-v] [-m model] [-d model_dir] video"
+    echo "       ${SCRIPT_NAME} [-v] [-m model] [-d model_dir] [-i] video"
     echo "${SCRIPT_NAME} converts the speech of a video to text."
     echo "The text is saved in a srt file with a suffix '.srt'"
     echo "appended to the name of input video"
@@ -19,12 +19,14 @@ function show_usage {
     echo "               video with a name equaling to video's name + .srt suffix."
     echo
     echo "  options:"
+    echo "  -d model_dir Specify the location to store whisper models."
+    echo "               Default to ${ROOT_DIR}/models."
     echo "  -m model     Specify whisper asr model to use. The full model list is"
     echo "               [tiny.en tiny base.en base small.en small medium.en medium"
     echo "                large-v1 large-v2 large]."
     echo "               Default to small."
-    echo "  -d model_dir Specify the location to store whisper models."
-    echo "               Default to ${ROOT_DIR}/models."
+    echo "  -i           Enable network. By default network is disabled."
+    echo "               '-i' must be specified when you need download AI model."
 }
 
 
@@ -38,10 +40,11 @@ fi
 VERBOSE=''
 MODEL=small
 MODEL_DIR=${ROOT_DIR}/models
+NET_ENABLED=false
 
 # Reset in case getopts has been used previously in the shell.
 OPTIND=1
-while getopts "hvm:d:" opt; do
+while getopts "hvm:d:i" opt; do
     case ${opt} in
         h ) # process option h
             show_usage
@@ -52,6 +55,9 @@ while getopts "hvm:d:" opt; do
             ;;
         m ) # process option m
             MODEL=${OPTARG}
+            ;;
+        i ) # process option i
+            NET_ENABLED=true
             ;;
         v ) # process option v
             VERBOSE='-v'
@@ -70,6 +76,7 @@ VIDEO_NAME=$(basename ${VIDEO})
 VIDEO_DIR=$(dirname ${VIDEO})
 SRT_FILE=${VIDEO}.srt
 SRT_FILE_NAME=$(basename ${SRT_FILE})
+DOCER_SERVICE=subtitler
 CWD=$(pwd)
 
 # create an empty srt file to be mounted by docker
@@ -77,6 +84,10 @@ if ! touch ${SRT_FILE}; then
     exit 1
 fi
 
+if ${NET_ENABLED}; then
+    DOCER_SERVICE=${DOCER_SERVICE}-with-net
+fi
+
 cd ${ROOT_DIR}
-docker-compose -f docker/docker-compose.yml run --rm -v ${VIDEO}:/video/${VIDEO_NAME}:ro -v ${SRT_FILE}:/output/${SRT_FILE_NAME} -v ${MODEL_DIR}:/models subtitler /app/retrieve.py ${VERBOSE} -m ${MODEL} -md /models /video/${VIDEO_NAME} /output
+docker-compose -f docker/docker-compose.yml run --rm -v ${VIDEO}:/video/${VIDEO_NAME}:ro -v ${SRT_FILE}:/output/${SRT_FILE_NAME} -v ${MODEL_DIR}:/models ${DOCER_SERVICE} /app/retrieve.py ${VERBOSE} -m ${MODEL} -md /models /video/${VIDEO_NAME} /output
 cd ${CWD}
